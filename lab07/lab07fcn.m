b8658ce0957f32080b12c4d0a593aecf720c3950
Reggie DePiero <rdepiero@uwyo.edu>
2018-03-07 15:04:44 -0700

    added lab07 pdf files

--- lab07/lab07fcn.m
+++ lab07/lab07fcn.m
@@ -1,4 +1,4 @@
-function [ x, iters ] = gsSOR( A, b, x0, w, TOL, maxIters  )
+function [ sol, iters ] = gsSOR( A, b, x0, z, toleranceForSystem, mi  )
 
 [r,c] = size(A); n = length(b); x = x0;
 if ( r ~= n ) || (c ~= n) disp('Error');
@@ -10,7 +10,7 @@ fprintf('---------------------------------------------------\n')
 fprintf(' iter | %8s | %8s | %8s | %8s | \n','x','y','z','res')
 fprintf('---------------------------------------------------\n')
 fprintf(' %4d | %8.5f | %8.5f | %8.5f | %8.5f | \n', 0, x(0), x(1), x(2), res)
-for k = 1:maxIters
+for k = 1:mi
 for i = 1:n
 s1 = 0;
 s2 = 0;
@@ -19,13 +19,14 @@ for j = (i+1):k
 s2 = s2 + A(i,j) * x0(j);
 fprintf(' %4d | %8.5f | %8.5f | %8.5f | %8.5f |\n', k, x(1), x(2), x(3), res)
 end
-x(i) = (1-w)*x0(i) + (w*( b(i) - s2 - s1 )) / A(i,i);
+x(i) = (1-z)*x0(i) + (z*( b(i) - s2 - s1 )) / A(i,i);
 end
 r = A*x - b;
-res = norm(k); if norm(x-x0) < TOL break;
+res = norm(k); if norm(x-x0) < tolerancForSystem break;
 end
 x=x0;
 end
+sol = x;
 fprintf('---------------------------------------------------\n')
 end
 
--- lab07/lab07script.m
+++ lab07/lab07script.m
@@ -5,7 +5,6 @@ clear
 A = [4 3 0;
     2 4 -1;
     0 -1 4];
-
 b = [24; 30; -24];
 
 TOL = 10^(-8)
@@ -19,5 +18,4 @@ fprintf('Solution of System: \n')
 fprintf('x1 = %1.5f \n',x(1))
 fprintf('x2 = %1.5f \n',x(2)) 
 fprintf('x3 = %1.5f\n',x(3))
-% fprintf('x5 = %1.5f\n',x(5))
 fprintf('found in %d iterations\n',iter)
