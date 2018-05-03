52311c632d401d46e504694e367b96dc23a44864
Reggie DePiero <rdepiero@uwyo.edu>
2018-05-03 14:07:45 -0600

    added lab13

---
+++ lab13/lab13_MonteCarlo1.m
@@ -0,0 +1,3 @@
+% Math 3341, Spring 2018
+% Lab 13 Monte Carlo Integration
+
---
+++ lab13/lab13_histograms.m
@@ -0,0 +1,33 @@
+% Math 3341, Spring 2018
+% Lab 13 Histograms
+% Generates histograms of random data & normal random data
+
+clear; close all;
+
+
+% generate random data
+
+
+
+% % generate figure with histograms
+% f1 = figure(1);
+% set(gcf, 'Units', 'Normalized', 'OuterPosition', [0.25 0 1 0.5]);
+% 
+% % histogram of random data between a & b
+% s1 = subplot(1,2,1);
+% 
+% 
+% title('Histogram using rand(N)','FontSize',14)
+% xlabel('value','FontSize',12), ylabel('count','FontSize',12)
+% 
+% % histogram of normal data
+% s2 = subplot(1,2,2);
+% 
+% title('Histogram using randn(N)','FontSize',14)
+% xlabel('value','FontSize',12), ylabel('count','FontSize',12)
+% % xlim([-5 5] )
+% 
+% linkaxes([s2,s1],'xy'); 
+% % % 
+% saveas(f1, 'histogramPlots', 'epsc')
+
